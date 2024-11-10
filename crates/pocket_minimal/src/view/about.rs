786004d9783f8484f535489e00cc0ff7f5d05c3c
Katherine Eaton <ktmeaton@gmail.com>
2024-11-10 14:14:05 -0700

    minimal: toc work

--- crates/pocket_minimal/src/view/about.rs
+++ crates/pocket_minimal/src/view/about.rs
@@ -1,4 +1,5 @@
-use egui::{collapsing_header::CollapsingState, CollapsingHeader, Ui};
+use egui::Ui;
+use egui::collapsing_header::{CollapsingState, HeaderResponse, paint_default_icon};
 
 pub struct About {
     table_of_contents: Tree,
@@ -7,12 +8,14 @@ pub struct About {
 
 impl About {
     fn create_table_of_contents() -> Tree {
-        let subtree1 = Tree::new("Section 1", vec!["Section 1.2"]);
-        let subtree2 = Tree::new("Section 2", vec![]);
-        Tree {
-            root: "Table of Contents".to_string(),
-            children: vec![subtree1, subtree2]
-        }
+        let mut tree = Tree::new("Table of Contents", vec![]);
+        tree.add_child("Section 1", "Table of Contents");
+        tree.add_child("Section 1.1", "Section 1");
+        tree.add_child("Section 1.1.1", "Section 1.1");
+        tree.add_child("Section 1.2", "Section 1");
+        tree.add_child("Section 2", "Table of Contents");
+        tree.add_child("Section 2.1", "Section 2");
+        tree
     }
 }
 
@@ -20,7 +23,7 @@ impl Default for About {
     fn default() -> Self {
         Self {
             table_of_contents: About::create_table_of_contents(),
-            selected: "Table of Contents".to_string()
+            selected: "Body".to_string()
         }
     }
 }
@@ -36,7 +39,8 @@ impl eframe::App for About {
                     ui.heading("Table of Contents");
                 });
                 ui.separator();
-                self.table_of_contents.ui(ui, &self.selected);
+                let (show_root, default_depth) = (false, 1);
+                self.selected = self.table_of_contents.ui(ui, show_root, default_depth, &self.selected);
             });
         // Central Panel: Articles
         // egui::CentralPanel::default()
@@ -58,41 +62,86 @@ impl Default for Tree {
 
 impl Tree {
 
+
+    pub fn add_child(&mut self, child: &str, parent: &str) {
+        if &self.root == parent {
+            self.children.push(Tree::new(child, vec![]))
+        } else {
+            self.children.iter_mut().for_each(|c| {c.add_child(child, parent)});
+        }
+
+    }
+
     pub fn new(root: &str, children: Vec<&str>) -> Self {
         Self {
             root: root.to_string(),
             children: children.iter().map(|c| Tree::new(c, vec![])).collect()
         }
     }
-    pub fn ui<'s>(&self, ui: &mut Ui, selected: &'s str) -> &'s str{
-        let selected = self.ui_recursive(ui, 0, &self.root, selected);
+
+    pub fn ui(&self, ui: &mut Ui, show_root: bool, default_depth: u32, selected: &str) -> String {
+        // Keep track of why tree button is selected
+        let mut selected = selected.to_string();
+
+        // If we don't want to show the root, we'll start the tree by iterating
+        // over children
+        let trees = match show_root {
+            true  => vec![self],
+            false => self.children.iter().collect(),
+        };
+        // Render each tree(s) UI
+        trees.into_iter().enumerate().for_each(|(i, tree)| {
+            let (depth, name, prefix) = (0, &tree.root, &vec![i as u32]);
+            selected = tree.ui_recursive(ui, depth, name, default_depth, &selected, prefix);
+        });
+
+        // Return the tree button that is selected
         return selected
     }
 
-    fn ui_recursive<'s>(&self, ui: &mut Ui, depth: usize, name: &str, selected: &'s str) -> &'s str {
+    fn ui_recursive(&self, ui: &mut Ui, depth: u32, name: &str, default_depth: u32, selected: &str, prefix: &[u32]) -> String {
 
-        let response = match self.children.len() > 0 {
-            true => {
-                let id = ui.make_persistent_id(format!("tree_{name}"));
-                CollapsingState::load_with_default_open(ui.ctx(), id, false)
-                    .show_header(ui, |ui| {
-                        ui.selectable_label(selected == name, name);
-                    })
-                    .body(|ui| {
-                        self
-                            .children
-                            .iter()
-                            .for_each(|child| { child.ui_recursive(ui, depth + 1, &child.root, selected);});
-                    })
-                    .0
-            },
-            false => ui.selectable_label(selected == name, name)
-        };
-        response.context_menu(|ui| {ui.label("Shown on right-clicks");});
+        // If the user selects a new tree button
+        let mut selected = selected.to_string();
 
-        match response.clicked(){
-            true => return name,
-            false => return selected,
+        // Configure a numeric prefix (ex. "1", "1.1", "2.3.1")
+        let mut prefix = prefix.to_vec();
+        if prefix.len() > 0 {
+            let l = prefix.len() - 1;
+            prefix[l] = prefix[l] + 1;
         }
+
+        // Create a new collapsing element
+        let mut state = CollapsingState::load_with_default_open(
+            ui.ctx(), 
+            ui.make_persistent_id(format!("tree_{name}")), 
+            depth <= 1
+        );
+
+        // THIS WORKS BELOW! BUT WE WANT: CUSTOM ICONS AND DEFAULT OPEN/CLOSE
+        // Configure the header
+        let header = state.show_header(ui, |ui| {
+            ui.horizontal(|ui| {
+                ui.label(prefix.iter().map(|i| i.to_string()).collect::<Vec<String>>().join("."));
+                let button = ui.selectable_label(selected == name, name);
+                button.context_menu(|ui| {ui.label("You right clicked a header!");});
+                if button.clicked() { selected = name.to_string();};
+            })
+        });
+        // Configure the body
+        let (collapse, _, _) = header.body(|ui| {
+            self
+                .children
+                .iter()
+                .enumerate()
+                .for_each(|(i, c)| {
+                    let c_prefix = prefix.clone().into_iter().chain([i as u32]).collect::<Vec<u32>>();
+                    let (c_depth, c_name) = (depth + 1, &c.root);
+                    selected = c.ui_recursive(ui, c_depth, c_name, default_depth, &selected, &c_prefix);
+                })
+        });
+        collapse.context_menu(|ui| {ui.label("You right-clicked the collapse icon!");});
+
+        return selected.to_string()
     }
 }
